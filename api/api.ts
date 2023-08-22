cac888921948d111ebe2d2c827d0a2c198ecadff
litury <litury@icloud.com>
2023-08-22 15:04:16 +0500

    feat: add amvera.yml and update api.ts

--- api/api.ts
+++ api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
 api.interceptors.response.use(
