e13c812dd3cac5c845153711ec75ed28575960f4
litury <litury@icloud.com>
2023-08-25 20:11:31 +0500

    split projeck

--- server/telegramBot.js
+++ server/telegramBot.js
@@ -4,7 +4,7 @@ const TOKEN = '6519194051:AAG2kj0L7zwYCl0K3eyfQT2DCtMkoH7Nk6Y';
 
 const bot = new TelegramBot(TOKEN, {polling: false}); // Отключаем polling
 
-const WEBHOOK_URL = 'http://1889059-ps47079.twc1.net/telegram';
+const WEBHOOK_URL = 'https://1889059-ps47079.twc1.net/telegram';
 
 bot.setWebHook(WEBHOOK_URL);
 
